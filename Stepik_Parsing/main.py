2f6e8aef738ef61ab786a3041d1e03d90adefc39
lizard <alex.lz11@yandex.ru>
2023-11-18 01:06:03 +0700

    2023-11-18 lz

--- Stepik_Parsing/main.py
+++ Stepik_Parsing/main.py
@@ -1,7 +1,20 @@
-import requests
+# import requests
 
-url = 'https://parsinger.ru/video_downloads/videoplayback.mp4'
-resp = requests.get(url, stream=True)
-with open('Stepik_Parsing/video.mp4', 'wb') as ouf:
-    for bit in resp.iter_content(chunk_size=1000000):
-        ouf.write(bit)
+# url = 'https://parsinger.ru/video_downloads/videoplayback.mp4'
+# resp = requests.get(url, stream=True)
+# with open('Stepik_Parsing/video.mp4', 'wb') as ouf:
+#     for bit in resp.iter_content(chunk_size=1000000):
+#         ouf.write(bit)
+
+import requests
+import time
+
+total = 0
+start = time.time()
+with requests.Session() as rs:
+    for i in range(200):
+        response = rs.head(f'https://parsinger.ru/3.3/2/{i+1}.html')
+        total += response.status_code
+stop = time.time()
+t = stop - start
+print(f'Сумма статусов = {total}\nВремя выполнения = {t}')
