88565842dd0074b35e84f6ff16ce23c9b0af5a94
lizard <alex.lz11@yandex.ru>
2024-01-30 00:15:07 +0700

    2024-01-29 lz

--- Stepik_Parsing/main.py
+++ Stepik_Parsing/main.py
@@ -763,27 +763,41 @@
 
 # {'giveSum': '4.1895414', 'getSum': '0.25619551'}
 
-import requests
+# import requests
 
 # headers = {'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0',
 #            'X-Requested-With': 'XMLHttpRequest', }
-headers = {'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0'}
+# headers = {'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0'}
 
-url = 'https://cbr.ru/Queries/AjaxDataSource/112805'
+# url = 'https://cbr.ru/Queries/AjaxDataSource/112805'
 
-data_dollar = {
-    'DT': '',
-    'val_id': 'R01235',
-    '_': '1667219511852'
-}
-data_euro = {
-    'DT': '',
-    'val_id': 'R01239',
-    '_': '1667219511853'
-}
-response_dollar = requests.get(url=url, headers=headers, params=data_dollar).json()[-1]
-response_euro = requests.get(url=url, headers=headers, params=data_euro).json()[-1]
+# data_dollar = {
+#     'DT': '',
+#     'val_id': 'R01235',
+#     '_': '1667219511852'
+# }
+# data_euro = {
+#     'DT': '',
+#     'val_id': 'R01239',
+#     '_': '1667219511853'
+# }
+# response_dollar = requests.get(url=url, headers=headers, params=data_dollar).json()[-1]
+# response_euro = requests.get(url=url, headers=headers, params=data_euro).json()[-1]
 
-print(f'Дата: {response_dollar["data"][:10]}')
-print(f'Курс USD: {response_dollar["curs"]} рублей')
-print(f'Курс EUR: {response_euro["curs"]} рублей')
+# print(f'Дата: {response_dollar["data"][:10]}')
+# print(f'Курс USD: {response_dollar["curs"]} рублей')
+# print(f'Курс EUR: {response_euro["curs"]} рублей')
+
+# 4.8 Парсинг табличных данных
+#********************************
+
+# 4.8.1 Агрегация уникальных данных из таблицы
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://parsinger.ru/table/1/index.html'
+html = requests.get(url)
+html.encoding = 'utf-8'
+soup = BeautifulSoup(html.text, 'lxml')
+numbers = {float(cell.text) for row in soup.select('tr')[1:] for cell in row.select('td')}
+print(sum(numbers))
