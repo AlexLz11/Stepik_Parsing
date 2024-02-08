5a783bcf9225ee4d2cb92780400d19ce45f5d5ee
lizard <alex.lz11@yandex.ru>
2024-02-08 23:01:59 +0700

    2024-02-08 lz

--- Stepik_Parsing/main.py
+++ Stepik_Parsing/main.py
@@ -1026,34 +1026,58 @@
 #             wr.writerow(data)
 
 # 4.9.3 Сбор данных со всех карточек(160шт)
+# import requests
+# import csv
+# from bs4 import BeautifulSoup
+
+# def get_soup(url, parser='lxml'):
+#     html = rs.get(url)
+#     html.encoding = 'utf-8'
+#     return BeautifulSoup(html.text, parser)
+
+# def card_data(card):
+#     item = card.select_one('a.name_item').text.strip()
+#     descr = [i.text.split(':')[1].strip() for i in card.select('li')]
+#     price = card.select_one('p.price').text.strip()
+#     return [item, *descr, price]
+
+# scheme = 'https://parsinger.ru/html/'
+# url = 'https://parsinger.ru/html/index1_page_1.html'
+# cards = []
+# with requests.Session() as rs:
+#     soup = get_soup(url)
+#     sections = [scheme + a['href'] for a in soup.select_one('div.nav_menu').select('a')]
+#     for sect in sections:
+#         soup = get_soup(sect)
+#         pages = [scheme + a['href'] for a in soup.select_one('div.pagen').select('a')]
+#         for page in pages:
+#             soup = get_soup(page)
+#             cards.extend(soup.select('div.item'))
+# with open('Stepik_Parsing/GoodsInfo.csv', 'w', encoding='utf-8-sig', newline='') as ouf:
+#     writer = csv.writer(ouf, delimiter=';')
+#     for card in cards:
+#         writer.writerow(card_data(card))
+
+# 4.9.4 Рецензируйте код других учеников
 import requests
 import csv
 from bs4 import BeautifulSoup
 
-def get_soup(url, parser='lxml'):
-    html = rs.get(url)
+def get_soup(url, session=None, parser='lxml'):
+    html = session.get(url) if session else requests.get(url)
     html.encoding = 'utf-8'
     return BeautifulSoup(html.text, parser)
 
-def card_data(card):
-    item = card.select_one('a.name_item').text.strip()
-    descr = [i.text.split(':')[1].strip() for i in card.select('li')]
-    price = card.select_one('p.price').text.strip()
-    return [item, *descr, price]
-
-scheme = 'https://parsinger.ru/html/'
-url = 'https://parsinger.ru/html/index1_page_1.html'
-cards = []
-with requests.Session() as rs:
-    soup = get_soup(url)
-    sections = [scheme + a['href'] for a in soup.select_one('div.nav_menu').select('a')]
-    for sect in sections:
-        soup = get_soup(sect)
-        pages = [scheme + a['href'] for a in soup.select_one('div.pagen').select('a')]
+def get_cards(url, session=None):
+    cards = []
+    scheme = 'https://parsinger.ru/html/'
+    soup = get_soup(url, session)
+    categories = [a['href'] + scheme for a in soup.select_one('div.nav_menu').select('a')]
+    for category in categories:
+        soup = get_soup(category, session)
+        pages = [a['href'] + scheme for a in soup.select_one('div.pagen').select('a')]
         for page in pages:
-            soup = get_soup(page)
-            cards.extend(soup.select('div.item'))
-with open('Stepik_Parsing/GoodsInfo.csv', 'w', encoding='utf-8-sig', newline='') as ouf:
-    writer = csv.writer(ouf, delimiter=';')
-    for card in cards:
-        writer.writerow(card_data(card))
+            
+
+
+url = 'https://parsinger.ru/html/index1_page_1.html'
