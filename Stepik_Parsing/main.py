b492e6d77c943702e2ac2bcd5af419b2d5ca282c
AlexLz11 <alex.lz11@yandex.ru>
2024-03-02 15:18:58 +0000

    2024-03-02 repl

--- Stepik_Parsing/main.py
+++ Stepik_Parsing/main.py
@@ -145,12 +145,23 @@
 #         print(f"Превышен timeout ожидания для - {PROXY}")
 #         continue
 
+# import time
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+
+# browser = webdriver.Chrome()
+# browser.get('http://parsinger.ru/html/watch/1/1_1.html')
+# button = browser.find_element(By.ID, "sale_button").click()
+
+# time.sleep(10)
+
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-browser = webdriver.Chrome()
-browser.get('http://parsinger.ru/html/watch/1/1_1.html')
-button = browser.find_element(By.ID, "sale_button").click()
-
-time.sleep(10)
+with webdriver.Chrome() as driver:
+    driver.get('http://parsinger.ru/html/watch/1/1_1.html')
+    button = driver.find_element(By.ID, "sale_button")
+    time.sleep(2)
+    button.click()
+    time.sleep(2)
