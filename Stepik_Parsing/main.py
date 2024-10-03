75a82bb419a0e62438494a6a08f2b0db765338be
AlexLz11 <alex.lz11@yandex.ru>
2024-10-03 17:09:16 +0000

    2024-10-03 repl

--- Stepik_Parsing/main.py
+++ Stepik_Parsing/main.py
@@ -57,28 +57,98 @@
 
 # asyncio.run(main())
 
-import asyncio
-import random
-import time
+# import asyncio
+# import random
+# import time
 
-async def one():
-    # Получаем текущее время в секундах с начала эпохи. 
-    start = time.time()
-    await asyncio.sleep((sleep_time := random.randint(1, 3)))
-    # Получаем имя текущей задачи и выводим сообщение с временем ее выполнения:
-    print(f'{asyncio.current_task().get_name()} ({sleep_time=}) выполнена за {time.time() - start}')
+# async def one():
+#     # Получаем текущее время в секундах с начала эпохи. 
+#     start = time.time()
+#     await asyncio.sleep((sleep_time := random.randint(1, 3)))
+#     # Получаем имя текущей задачи и выводим сообщение с временем ее выполнения:
+#     print(f'{asyncio.current_task().get_name()} ({sleep_time=}) выполнена за {time.time() - start}')
+
+# async def main():
+#     # Создание списка задач.
+#     lst_tasks = []
+#     for x in range(10):
+#         # Корутины должны быть явно обернуты в Task.
+#         task = asyncio.create_task(one(), name=f'Задача_{x}')
+#         lst_tasks.append(task)
+#     done, pending = await asyncio.wait(lst_tasks, timeout=2)
+#     print(f'Не успели выполниться: {[task.get_name() for task in pending]}')
+#     print(done, pending, sep='\n')
+#     # Даем время выполниться оставшимся задачам
+#     await asyncio.sleep(3)
+
+# asyncio.run(main())
+
+# import asyncio
+
+# import aiohttp
+
+
+# async def main():
+#     async with aiohttp.ClientSession(trust_env=True) as session:
+#         async with session.get('https://parsinger.ru/html/index1_page_1.html') as response:
+#             print(await response.text())
+
+# asyncio.run(main())
+
+# import aiohttp
+# import asyncio
+
+# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'}
+# url = 'http://httpbin.org/get'
+# data = {'sessionKey': '9ebbd0b25760557393a43064a92bae539d962103', 'format': 'xml', 'platformId': 1}
+
+# async def main():
+#     async with aiohttp.ClientSession(trust_env=True) as session:
+#         async with session.get(url=url, headers=headers, timeout=2, params=data) as response:
+#             print(await response.text())
+
+# asyncio.run(main())
+
+import time
+import aiohttp
+import asyncio
+import aiofiles
+
+url = 'http://httpbin.org/ip'
+
+
+async def check_proxy(prx, semaphore):
+    proxy = f'http://{prx}'
+    async with semaphore:
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url=url, proxy=proxy, timeout=1) as response:
+                    if response.ok:
+                        return f'good proxy, status_code: {response.status}, {prx}'
+                    else:
+                        return f'bad proxy, status_code: {response.status}, {prx}'
+        except Exception as e:
+            return f'bad proxy, Error: {e.__class__.__name__}, {prx}'
+
 
 async def main():
-    # Создание списка задач.
-    lst_tasks = []
-    for x in range(10):
-        # Корутины должны быть явно обернуты в Task.
-        task = asyncio.create_task(one(), name=f'Задача_{x}')
-        lst_tasks.append(task)
-    done, pending = await asyncio.wait(lst_tasks, timeout=2)
-    print(f'Не успели выполниться: {[task.get_name() for task in pending]}')
-    print(done, pending, sep='\n')
-    # Даем время выполниться оставшимся задачам
-    await asyncio.sleep(3)
+    # Внимание! Этот оператор ограничивает количество одновременно выполняемых задач
+    # Если код падает с ошибкой ValueError: too many file descriptors in select(), 
+    # уменьшите это число
+    semaphore = asyncio.BoundedSemaphore(500)
 
+    async with aiofiles.open('Stepik_Parsing/proxy1.txt', mode='r', encoding='utf8') as f:
+        # Получаем список прокси из файла
+        proxies = await f.readlines()
+        # Создаем и асинхронно запускаем список задач на проверку прокси
+        tasks = [check_proxy(prx.strip(), semaphore) for prx in proxies]
+        # Ждем, пока выполнятся все проверки
+        result = await asyncio.gather(*tasks, return_exceptions=True)
+        # Выводим результат проверки
+        print(f"Всего проверено: {len(result)} шт.")
+        print(*result, sep='\n')
+
+
+start = time.time()
 asyncio.run(main())
+print(f'Затрачено времени: {time.time() - start} секунд')
