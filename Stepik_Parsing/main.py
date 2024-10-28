8b7bffba9ed9a859bd88877da7e4f7696236ee83
lizard <alex.lz11@yandex.ru>
2024-10-28 23:29:50 +0700

    2024-10-28 lz

--- Stepik_Parsing/main.py
+++ Stepik_Parsing/main.py
@@ -6,23 +6,23 @@
 
 # print(pyrogram.__version__)
 
-# from pyrogram import Client
+from pyrogram import Client
 
-# api_id = 25779997
-# api_hash = "93c2608907a0ed17a4fa0973bcc65ee3"
-# group_url = "python_parsing"
-# app = Client("my_session", api_id=api_id, api_hash=api_hash)
-
-
-# def main():
-#     with app:
-#         all_messages = []
-#         for message in app.get_chat_history(group_url, limit=100):
-#             all_messages.append(message.text)
-
-#         # Вывод сообщений на экран или сохранение в файл
-#         for msg in all_messages:
-#             print(msg)
+api_id = 25779997
+api_hash = "93c2608907a0ed17a4fa0973bcc65ee3"
+group_url = "python_parsing"
+app = Client("my_session", api_id=api_id, api_hash=api_hash)
 
 
-# main()
+def main():
+    with app:
+        all_messages = []
+        for message in app.get_chat_history(group_url, limit=100): # type: ignore
+            all_messages.append(message.text)
+
+        # Вывод сообщений на экран или сохранение в файл
+        for msg in all_messages:
+            print(msg)
+
+
+main()
