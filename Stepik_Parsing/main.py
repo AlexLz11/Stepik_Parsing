3032ca376baf6dd9e2347a4550c68fa35659b70f
lizard <alex.lz11@yandex.ru>
2024-10-30 23:13:04 +0700

    2024-10-30 lz

--- Stepik_Parsing/main.py
+++ Stepik_Parsing/main.py
@@ -6,6 +6,8 @@
 
 # print(pyrogram.__version__)
 
+# Первый запуск
+
 # from pyrogram import Client
 # import json
 
@@ -30,3 +32,31 @@
 
 
 # main()
+
+# Первый запуск async
+
+import asyncio
+from pyrogram import Client
+import json
+
+with open('Stepik_Parsing/api_keys.json') as file:
+    api_keys = json.load(file)
+
+api_id = api_keys['api_id']
+api_hash = api_keys['api_hash']
+group_url = "python_parsing"
+
+async def get_all_messages(app, group_url, limit=100):
+    all_messages = []
+    async for message in app.get_chat_history(group_url, limit=limit):
+        all_messages.append(message.text)
+    return all_messages
+
+async def main():
+    app = Client("my_session", api_id=api_id, api_hash=api_hash)
+    async with app:
+        messages = await get_all_messages(app, group_url, limit=100)
+        for msg in messages:
+            print(msg)
+
+asyncio.run(main())
