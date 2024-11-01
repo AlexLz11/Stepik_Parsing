6a65e898b5a6ae334d9007424cb61682ca86677e
lizard <alex.lz11@yandex.ru>
2024-11-01 23:24:37 +0700

    2024-11-01 lz

--- Stepik_Parsing/main.py
+++ Stepik_Parsing/main.py
@@ -231,3 +231,86 @@
 
 # if __name__ == '__main__':
 #     main()
+
+# 7.3.1. Исследователь структур и точечной нотации
+
+# from pyrogram import Client
+# import json
+
+# with open('Stepik_Parsing/api_keys.json') as file:
+#     api_keys = json.load(file)
+
+# api_id = api_keys['api_id']
+# api_hash = api_keys['api_hash']
+# group_url = "parsinger_pyrogram"
+
+# def main():
+#     app = Client('my_session', api_id=api_id, api_hash=api_hash)
+#     with app:
+#         chat = app.get_chat(group_url)
+#         with open('Stepik_Parsing/chat_data.json', 'w') as file:
+#             print(chat, file=file)
+
+# main()
+
+# 7.3.2. В поисках идентификатора чата
+
+# from pyrogram import Client
+# import json
+
+# with open('Stepik_Parsing/api_keys.json') as file:
+#     api_keys = json.load(file)
+
+# api_id = api_keys['api_id']
+# api_hash = api_keys['api_hash']
+# group_url = "parsinger_pyrogram"
+
+# def main():
+#     app = Client('my_session', api_id=api_id, api_hash=api_hash)
+#     with app:
+#         chat = app.get_chat(group_url)
+#         print(chat.id)
+
+# main()
+
+# 7.3.3. Поиск уникального идентификатора аватара группы
+
+# from pyrogram import Client
+# import json
+
+# with open('Stepik_Parsing/api_keys.json') as file:
+#     api_keys = json.load(file)
+
+# api_id = api_keys['api_id']
+# api_hash = api_keys['api_hash']
+# group_url = "parsinger_pyrogram"
+
+# def main():
+#     app = Client('my_session', api_id=api_id, api_hash=api_hash)
+#     with app:
+#         chat = app.get_chat(group_url)
+#         print(chat.photo.big_photo_unique_id)
+
+# main()
+
+# 7.3.4. Охота за сокровищами в цифровых глубинах
+
+from pyrogram import Client
+import json
+
+with open('Stepik_Parsing/api_keys.json') as file:
+    api_keys = json.load(file)
+
+api_id = api_keys['api_id']
+api_hash = api_keys['api_hash']
+group_url = "parsinger_pyrogram"
+
+def main():
+    app = Client('my_session', api_id=api_id, api_hash=api_hash)
+    with app:
+        chat = app.get_chat(group_url)
+        description = chat.description
+        jem = ''.join([x for x in description if x.isdigit() and int(x)>0 and int(x)%2 == 0])
+        print(jem)
+
+main()
